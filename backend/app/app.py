4a139c41a57b299454fa8084af813cebbb8102c9
Konstantinos Agathopoulos <kostaga203@gmail.com>
2024-12-01 17:30:28 +0200

    tf_idf

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,8 +1,9 @@
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from scripts.stopwords import STOPWORDS
-from scripts.dataCleaning  import remove_punctuation_and_numbers, stem_words, to_lowercase
+from scripts.dataCleaning import to_lowercase, remove_punctuation_and_numbers, stem_words
 
 # Create a Flask app
 app = Flask(__name__)
@@ -15,22 +16,23 @@ CORS(app)  # Enable CORS for connecting with React
 def get_data():
     return jsonify({"message": "API is working!"})
 
-
 @app.route('/search', methods=['POST'])
 def search():
     data = request.json
     if not data:
-        return jsonify({"Error ": "Invalid JSON"}),400
+        return jsonify({"Error": "Invalid JSON"}), 400
     
     name = data.get('name', '')
     date = data.get('date', '')
     political_party = data.get('politicalParty', '')
     keywords = data.get('keywords', '')
-    keywords = keywords.apply(to_lowercase)
-    keywords = keywords.apply(lambda x: ' '.join([remove_punctuation_and_numbers(word) for word in x.split()]))
-    keywords = keywords.apply(lambda x: ' '.join([stem_words(word) for word in x.split()]))
+
+    # Apply preprocessing functions
+    keywords = to_lowercase(keywords)
+    keywords = ' '.join([remove_punctuation_and_numbers(word) for word in keywords.split()])
+    keywords = ' '.join([stem_words(word) for word in keywords.split()])
     
-    print(name,date,political_party,keywords)
+    print(name, date, political_party, keywords)
 
     return jsonify({
         "name": name,
@@ -38,8 +40,6 @@ def search():
         "political_party": political_party,
         "keywords": keywords
     })
-  
-
 
 
 if __name__ == '__main__':
--- backend/app/scripts/inverted_index.py
+++ backend/app/scripts/inverted_index.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import os
 from dataCleaning import clean_dataset
+from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
+import math
 
 
 if not os.path.exists("cleaned_data.csv"):
@@ -9,11 +11,15 @@ if not os.path.exists("cleaned_data.csv"):
     df = pd.read_csv("data_sample.csv")
     clean_dataset(df)
     print("File created!")
-
-
-def create_inverted_index():
-    # Read the cleaned data
+else:
     df = pd.read_csv("cleaned_data.csv")
+
+
+
+def create_inverted_index() -> dict:
+    '''Creates an inverted index for the cleaned data and saves it to a pickle file'''
+    # Read the cleaned data
+   
     print("Creating the inverted index...")
     inverted_index = {}
 
@@ -21,7 +27,7 @@ def create_inverted_index():
         words = row['clean_speech'].split()
         for word in words:
             if word not in inverted_index:
-                # The inverse index catalogue maps words to a list of indexes containing the word and their term frequency.
+                # maps words to a list of indexes containing the word and their term frequency.
                 inverted_index[word] = {index: 1}
             else:
                 if index in inverted_index[word]:
@@ -32,10 +38,46 @@ def create_inverted_index():
 
     
     print("Inverted index created!")
-    print(inverted_index)
 
     with open('inverted_index.pkl', 'wb') as f:
         pickle.dump(inverted_index, f)
+    
+    return inverted_index
 
 
-create_inverted_index()
+
+def calculate_tf_idf(inverted_index: dict, total_documents: int) -> dict:
+    '''Calculates the term frequency-inverse document frequency for the cleaned data'''
+    tfidf = {}
+   
+    for word, indexes in inverted_index.items():
+        # calculate the idf
+        idf = math.log(total_documents / (1 + len(indexes)))
+
+        for index, term_count in indexes.items():
+            # calculate the tf
+            tf = 1 + math.log(term_count)
+            if index not in tfidf:
+                tfidf[index] = {word: tf * idf}
+            else:
+                tfidf[index][word] = tf * idf
+
+    return tfidf
+
+inverted_index = create_inverted_index()
+tf_idf = calculate_tf_idf(inverted_index, len(df['clean_speech']))
+print(tf_idf)
+print("-----------------")
+def calculate_tf_idf2():
+    '''Calculates the term frequency-inverse document frequency for the cleaned data'''
+    # create object
+    tfidf = TfidfVectorizer()
+    
+    # get tf-df values
+    tfidf.fit_transform(df['clean_speech'])
+    # get idf values
+    print('\nidf values:')
+    for ele1, ele2 in zip(tfidf.get_feature_names_out(), tfidf.idf_):
+        print(ele1, ':', ele2)
+# create_inverted_index()
+# calculate_tf_idf2()
